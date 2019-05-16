8cf98495f1bb761769299d8a7e5490b8a7fdf250
alex <471502979@qq.com>
2019-05-16 18:10:40 +0800

    update

--- common-library/src/test/java/com/github/common/io/SendFile_mmap_Test.java
+++ common-library/src/test/java/com/github/common/io/SendFile_mmap_Test.java
@@ -1,12 +1,9 @@
 package com.github.common.io;
 
 import com.google.common.base.Stopwatch;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
 import org.junit.Test;
-import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.io.*;
 import java.nio.ByteBuffer;
@@ -21,41 +18,99 @@ import java.util.concurrent.CountDownLatch;
  * 利用 IO 零拷贝的 MQ 们
  * sendFile：FileChannel，非零拷贝，基于 block
  * mmap：基于OS的mmap 的内存映射技术，通过 MMU 映射文件，使随机读写文件和读写内存相似的速度
- * 读 4kb 以下的数据请使用 mmap，大于 4kb请使用 FileChannel
+ * 读 4kb 以下的数据请使用 mmap，大于 4kb 以上请使用 FileChannel，该理论也看情况：要看读大于写还是反之
+ * 简单测试：分值计算规则(32 byte的cost+64 byte的cost)/2
+ * write(<4kb):  RandomAccessFile(4.1s)>FileChannel(3.9s)>FileOutputStream(3.9s)>MappedByteBuffer(67ms)
+ * read(<4kb):   RandomAccessFile(2.6s)>FileChannel(2.6s)>MappedByteBuffer(69ms)
+ * <p>
+ * write(>=4kb): FileChannel(150ms)>RandomAccessFile(83.5ms)>FileOutputStream(83ms)>MappedByteBuffer(50ms)
+ * read(>=4kb):  RandomAccessFile(197ms)>FileChannel(74ms)>MappedByteBuffer(72ms)
+ * <p>
+ * 从测试结论上看，4kb时使用FileChannel好像不太明显啊，这是由于windows系统是不支持sendfile的（可参阅FileChannelImpl.transferTo0），所以windows下实际走的是mmap
+ * <p>
+ * writeRandomAccessFile, 32.byte ,cost：5.420 s
+ * writeFileOutputStream, 32.byte ,cost：5.269 s
+ * writeFileChannel, 	   32.byte ,cost：5.284 s
+ * writeMappedByteBuffer, 32.byte ,cost：81.97 ms
+ * readRandomAccessFile,  32.byte ,cost：3.542 s
+ * readFileChannel,       32.byte ,cost：3.523 s
+ * readMappedByteBuffer,  32.byte ,cost：85.27 ms
+ * <p>
+ * writeRandomAccessFile, 64.byte ,cost：2.856 s
+ * writeFileOutputStream, 64.byte ,cost：2.691 s
+ * writeFileChannel,      64.byte ,cost：2.700 s
+ * writeMappedByteBuffer, 64.byte ,cost：52.61 ms
+ * readRandomAccessFile,  64.byte ,cost：1.807 s
+ * readFileChannel,       64.byte ,cost：1.769 s
+ * readMappedByteBuffer,  64.byte ,cost：51.21 ms
+ * <p>
+ * writeRandomAccessFile, 4096.byte ,cost：102.0 ms
+ * writeFileOutputStream, 4096.byte ,cost：96.19 ms
+ * writeFileChannel,      4096.byte ,cost：149.1 ms
+ * writeMappedByteBuffer, 4096.byte ,cost：54.84 ms
+ * readRandomAccessFile,  4096.byte ,cost：182.0 ms
+ * readFileChannel, 	   4096.byte ,cost：82.41 ms
+ * readMappedByteBuffer,  4096.byte ,cost：52.41 ms
+ * <p>
+ * writeRandomAccessFile, 8192.byte ,cost：65.04 ms
+ * writeFileOutputStream, 8192.byte ,cost：70.69 ms
+ * writeFileChannel, 	   8192.byte ,cost：152.7 ms
+ * writeMappedByteBuffer, 8192.byte ,cost：46.65 ms
+ * readRandomAccessFile,  8192.byte ,cost：213.4 ms
+ * readFileChannel, 	   8192.byte ,cost：67.68 ms
+ * readMappedByteBuffer,  8192.byte ,cost：92.38 ms
  * @Date: created in 2018/5/15.
  */
 public class SendFile_mmap_Test {
-    private long fileSize = 1024 * 1024 * 1024;
-    private static int[] lenArr = {32, 64, 128, 512, 1024, 2048, 4096, 8192, 16384, 134217728, 1073741824};
+    //50mb
+    private long fileSize = 1024 * 1024 * 100;
+    private File randomAccessFile;
+    private File outputStreamFile;
+    private File fileChannel;
+    private File mappedByteBufferFile;
+
+    @Before
+    public void init() throws IOException {
+        System.out.println("写文件大小为" + fileSize / 1024 / 1024 + "mb，每次写入32byte,64byte,4KB,8KB进行测试");
+        System.gc();
+        randomAccessFile = getFile("RandomAccessFile", true);
+        outputStreamFile = getFile("FileOutputStream", true);
+        fileChannel = getFile("FileChannel", true);
+        mappedByteBufferFile = getFile("MappedByteBuffer", true);
+    }
 
     @Test
     public void rwfile32() throws IOException {
         int byteSize = 32;
-        File file = writeRandomAccessFile(byteSize);
-        File file1 = writeFileOutputStream(byteSize);
-        File file2 = writeFileChannel(byteSize);
-        File file3 = writeMappedByteBuffer(byteSize);
-        readRandomAccessFile(file, byteSize);
-        readFileChannel(file2, byteSize);
-        readMappedByteBuffer(file3, byteSize);
+        writeRandomAccessFile(byteSize);
+        writeFileOutputStream(byteSize);
+        writeFileChannel(byteSize);
+        writeMappedByteBuffer(byteSize);
+        readRandomAccessFile(byteSize);
+        readFileChannel(byteSize);
+        readMappedByteBuffer(byteSize);
     }
+
+    //force 对性能影响很大，应该单独测试
     @Test
     public void writeAsyncForce32() throws IOException {
         int byteSize = 32;
         writeAsyncForceFileChannel(byteSize);
         writeAsyncForceMappedByteBuffer(byteSize);
     }
+
     @Test
     public void rwfile64() throws IOException {
         int byteSize = 64;
-        File file = writeRandomAccessFile(byteSize);
-        File file1 = writeFileOutputStream(byteSize);
-        File file2 = writeFileChannel(byteSize);
-        File file3 = writeMappedByteBuffer(byteSize);
-        readRandomAccessFile(file, byteSize);
-        readFileChannel(file2, byteSize);
-        readMappedByteBuffer(file3, byteSize);
+        writeRandomAccessFile(byteSize);
+        writeFileOutputStream(byteSize);
+        writeFileChannel(byteSize);
+        writeMappedByteBuffer(byteSize);
+        readRandomAccessFile(byteSize);
+        readFileChannel(byteSize);
+        readMappedByteBuffer(byteSize);
     }
+
     @Test
     public void writeAsyncForce64() throws IOException {
         int byteSize = 64;
@@ -66,56 +121,58 @@ public class SendFile_mmap_Test {
     @Test
     public void rwfile4096() throws IOException {
         int byteSize = 4096;
-        File file = writeRandomAccessFile(byteSize);
-        File file1 = writeFileOutputStream(byteSize);
-        File file2 = writeFileChannel(byteSize);
-        File file3 = writeMappedByteBuffer(byteSize);
-        readRandomAccessFile(file, byteSize);
-        readFileChannel(file2, byteSize);
-        readMappedByteBuffer(file3, byteSize);
+        writeRandomAccessFile(byteSize);
+        writeFileOutputStream(byteSize);
+        writeFileChannel(byteSize);
+        writeMappedByteBuffer(byteSize);
+        readRandomAccessFile(byteSize);
+        readFileChannel(byteSize);
+        readMappedByteBuffer(byteSize);
     }
+
     @Test
     public void writeAsyncForce4096() throws IOException {
         int byteSize = 4096;
         writeAsyncForceFileChannel(byteSize);
         writeAsyncForceMappedByteBuffer(byteSize);
     }
+
     @Test
     public void rwfile8192() throws IOException {
         int byteSize = 8192;
-        File file = writeRandomAccessFile(byteSize);
-        File file1 = writeFileOutputStream(byteSize);
-        File file2 = writeFileChannel(byteSize);
-        File file3 = writeMappedByteBuffer(byteSize);
-        readRandomAccessFile(file, byteSize);
-        readFileChannel(file2, byteSize);
-        readMappedByteBuffer(file3, byteSize);
+        writeRandomAccessFile(byteSize);
+        writeFileOutputStream(byteSize);
+        writeFileChannel(byteSize);
+        writeMappedByteBuffer(byteSize);
+        readRandomAccessFile(byteSize);
+        readFileChannel(byteSize);
+        readMappedByteBuffer(byteSize);
     }
+
     @Test
     public void writeAsyncForce8192() throws IOException {
         int byteSize = 8192;
         writeAsyncForceFileChannel(byteSize);
         writeAsyncForceMappedByteBuffer(byteSize);
     }
-    private File writeRandomAccessFile(int lenth) throws IOException {
+
+    private void writeRandomAccessFile(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        File file = getFile("RandomAccessFile");
-        if (!file.exists()) file.createNewFile();
-        RandomAccessFile ra = new RandomAccessFile(file, "r");
-        while (true) {
-            byte[] arr = new byte[lenth];
-            int byteSize = ra.read(arr);
-            if (byteSize == -1) {
-                break;
-            }
+        RandomAccessFile ra = new RandomAccessFile(randomAccessFile, "rw");
+        byte[] arr = new byte[lenth];
+        Arrays.fill(arr, (byte) 2);
+        int count = 0;
+        while (count < fileSize) {
+            count += arr.length;
+            ra.write(arr);
         }
         logFormat("writeRandomAccessFile", lenth, stopwatch);
-        return file;
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> IOUtils.closeQuietly(ra)));
     }
 
-    private void readRandomAccessFile(File file, int lenth) throws IOException {
+    private void readRandomAccessFile(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        RandomAccessFile ra = new RandomAccessFile(file, "r");
+        RandomAccessFile ra = new RandomAccessFile(randomAccessFile, "r");
         while (true) {
             byte[] arr = new byte[lenth];
             int len = ra.read(arr);
@@ -124,44 +181,53 @@ public class SendFile_mmap_Test {
             }
         }
         logFormat("readRandomAccessFile", lenth, stopwatch);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> IOUtils.closeQuietly(ra)));
     }
 
-    private File writeFileOutputStream(int lenth) throws IOException {
+    private void writeFileOutputStream(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        File file = getFile("FileOutputStream");
-        FileOutputStream fo = new FileOutputStream(file);
+        FileOutputStream fo = new FileOutputStream(outputStreamFile);
         byte[] arr = new byte[lenth];
         Arrays.fill(arr, (byte) 2);
-        int length = 0;
-        while (length < fileSize) {
-            length += arr.length;
+        int count = 0;
+        while (count < fileSize) {
+            count += arr.length;
             fo.write(arr);
         }
         //fo.flush();
         logFormat("writeFileOutputStream", lenth, stopwatch);
-        return file;
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> IOUtils.closeQuietly(fo)));
     }
 
-    private File writeFileChannel(int lenth) throws IOException {
+    private void writeFileChannel(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        File file = getFile("RandomAccessFile");
-        FileChannel fc = new RandomAccessFile(file, "rw").getChannel();
+        FileChannel fc = new RandomAccessFile(fileChannel, "rw").getChannel();
         byte[] arr = new byte[lenth];
         Arrays.fill(arr, (byte) 2);
-        int length = 0;
-        ByteBuffer b = ByteBuffer.wrap(arr);
-        while (length < fileSize) {
-            length += arr.length;
-            fc.write(b);
+        int count = 0;
+        ByteBuffer buf = ByteBuffer.wrap(arr);
+        while (count < fileSize) {
+            count += arr.length;
+            buf.clear();
+            buf.put(arr);
+            buf.flip();
+            while (buf.hasRemaining()) {
+                fc.write(buf);
+            }
         }
-        //fc.force(true);
         logFormat("writeFileChannel", lenth, stopwatch);
-        return file;
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                fc.force(false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
     }
 
-    private void readFileChannel(File file, int lenth) throws IOException {
+    private void readFileChannel(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        FileChannel fc = new RandomAccessFile(file, "rw").getChannel();
+        FileChannel fc = new RandomAccessFile(fileChannel, "rw").getChannel();
         ByteBuffer byteBuffer = ByteBuffer.allocate(lenth);
         while (true) {
             int len = fc.read(byteBuffer);
@@ -171,33 +237,39 @@ public class SendFile_mmap_Test {
             }
         }
         logFormat("readFileChannel", lenth, stopwatch);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                fc.force(false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
     }
 
-    private File writeMappedByteBuffer(int lenth) throws IOException {
+    private void writeMappedByteBuffer(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        File file = getFile("MappedByteBuffer");
-        MappedByteBuffer mb = new RandomAccessFile(file, "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
+        MappedByteBuffer mb = new RandomAccessFile(mappedByteBufferFile, "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
         byte[] arr = new byte[lenth];
         Arrays.fill(arr, (byte) 2);
-        int length = 0;
-        while (length < mb.capacity()) {
-            length += arr.length;
+        int count = 0;
+        while (count < mb.capacity()) {
+            count += arr.length;
             mb.put(arr);
         }
-        //mb.force();
         logFormat("writeMappedByteBuffer", lenth, stopwatch);
-        return file;
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> mb.force()));
     }
 
-    private void readMappedByteBuffer(File file, int lenth) throws IOException {
+    private void readMappedByteBuffer(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        MappedByteBuffer mb = new RandomAccessFile(file, "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
+        MappedByteBuffer mb = new RandomAccessFile(mappedByteBufferFile, "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
         byte[] arr = new byte[lenth];
         while (mb.hasRemaining()) {
             mb.get(arr);
             arr = new byte[lenth];
         }
         logFormat("readMappedByteBuffer", lenth, stopwatch);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> mb.force()));
     }
 
     private static volatile int curLen = 0;
@@ -205,34 +277,35 @@ public class SendFile_mmap_Test {
 
     private void writeAsyncForceFileChannel(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        File file = getFile("FileChannel");
-        FileChannel fc = new RandomAccessFile(file, "rw").getChannel();
+        FileChannel fc = new RandomAccessFile(fileChannel, "rw").getChannel();
         byte[] arr = new byte[lenth];
         Arrays.fill(arr, (byte) 2);
         CountDownLatch latch = new CountDownLatch(1);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (; ; ) {
-                    if (curLen < page4 && curLen % page4 == 0) {
-                        try {
-                            fc.force(false);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+        new Thread(() -> {
+            for (; ; ) {
+                if (curLen < page4 && curLen % page4 == 0) {
+                    try {
+                        fc.force(false);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                    if (latch.getCount() == 0) {
-                        break;
-                    }
+                }
+                if (latch.getCount() == 0) {
+                    break;
                 }
             }
         }).start();
-        ByteBuffer b = ByteBuffer.wrap(arr);
+        ByteBuffer buf = ByteBuffer.wrap(arr);
         while (curLen < fileSize) {
             curLen += arr.length;
-            fc.write(b);
+            buf.clear();
+            buf.put(arr);
+            buf.flip();
+            while (buf.hasRemaining()) {
+                fc.write(buf);
+            }
         }
-        curLen=0;
+        curLen = 0;
         latch.countDown();
         logFormat("writeAsyncForceFileChannel", lenth, stopwatch);
     }
@@ -241,21 +314,17 @@ public class SendFile_mmap_Test {
 
     private void writeAsyncForceMappedByteBuffer(int lenth) throws IOException {
         Stopwatch stopwatch = Stopwatch.createStarted();
-        File file = getFile("MappedByteBuffer");
-        MappedByteBuffer mb = new RandomAccessFile(file, "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
+        MappedByteBuffer mb = new RandomAccessFile(mappedByteBufferFile, "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
         byte[] arr = new byte[lenth];
         Arrays.fill(arr, (byte) 2);
         CountDownLatch latch = new CountDownLatch(1);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (; ; ) {
-                    if (curLen2 > page4 && curLen2 % page4 == 0) {
-                        mb.force();
-                    }
-                    if (latch.getCount() == 0) {
-                        break;
-                    }
+        new Thread(() -> {
+            for (; ; ) {
+                if (curLen2 > page4 && curLen2 % page4 == 0) {
+                    mb.force();
+                }
+                if (latch.getCount() == 0) {
+                    break;
                 }
             }
         }).start();
@@ -263,19 +332,19 @@ public class SendFile_mmap_Test {
             curLen2 += arr.length;
             mb.put(arr);
         }
-        curLen2=0;
+        curLen2 = 0;
         latch.countDown();
         logFormat("writeAsyncForceMappedByteBuffer", lenth, stopwatch);
     }
 
     private void logFormat(String method, int length, Stopwatch stopwatch) {
-        System.out.println(String.format("%s: %s.byte ,耗时：%s", method, length, stopwatch));
+        System.out.println(String.format("%s, %s.byte ,cost：%s", method, length, stopwatch));
     }
 
-    private File getFile(String type) {
-        String fileName = String.format("file-%s-%s.txt", type, System.currentTimeMillis());
+    private File getFile(String type, boolean isCreate) throws IOException {
+        String fileName = String.format("target/%s-%s.txt", type, System.currentTimeMillis());
         File file = new File(fileName);
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> file.delete()));
+        if (!file.exists()) file.createNewFile();
         return file;
     }
 }
